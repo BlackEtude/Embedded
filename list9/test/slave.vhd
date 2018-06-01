cff0f9a4edffede0c48fa2791b2fa240771afe93
BlackEtude <229726@student.pwr.edu.pl>
2018-06-01 18:18:09 +0200

    list_extra3 v1.0 - still needs some work

--- list9/test/slave.vhd
+++ list9/test/slave.vhd
@@ -1,15 +1,21 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+use std.textio.all;
+USE work.txt_util.ALL;
 
 ------------------------------
 --		STATES
 ------------------------------
 -- idle  = "000001"
--- start = "000000"
--- data  = "000010"
--- stop  = "000100"
--- write = "100000"
+-- write = "000010"
+-- send_start = "000011"
+-- send_data  = "000100"
+-- send_stop  = "000101"
+-- send_end = "00110"
+-- receive_start = "001010"
+-- receive_data  = "001011"
+-- receive_stop  = "001100"
 -- error = "111111"
 ------------------------------
 
@@ -24,62 +30,54 @@ entity slave is
 end slave;
 architecture Behavioral of slave is
 
-type state_type is (IDLE, START, DATA, STOP, ERROR, WRITE);
+type state_type is (IDLE, RECEIVE_START, RECEIVE_DATA, RECEIVE_STOP, 
+					ERROR, WRITE, SEND_START, SEND_STOP, SEND_DATA, SEND_END);
 signal current_s : state_type := IDLE;
 signal next_s : state_type := IDLE;
 
 -- input buffer
 signal data_in : std_logic_vector (7 downto 0) := (others => '0');
 signal control_in : std_logic_vector (7 downto 0) := (others => '0');
-signal command : std_logic_vector (7 downto 0) := (others => '0');
 
-signal result_reg : std_logic_vector (7 downto 0) := (others => '0');
 signal vstate : std_logic_vector(5 downto 0);
 signal sending : std_logic := '0';
+signal end_sending : std_logic := '0';
 
-constant clk_period : time := 10 ns;
-
--- data to send
+signal sender : std_logic_vector (7 downto 0) := (others => '0');
 signal id_tosend : std_logic_vector (7 downto 0) := (others => '0');
 signal data_tosend : std_logic_vector (7 downto 0) := (others => '0');
 
-procedure send(id, data, control_in : in std_logic_vector; signal data_bus, control_bus : out std_logic_vector) is
-	begin
-		--if control_in = "ZZZZZZZZ" then
-		--end if;
+signal result_reg : std_logic_vector (7 downto 0) := (others => '0');
+signal tmp_data_bus : std_logic_vector (7 downto 0) := (others => '0');
 
-		--while control_in = "ZZZZZZZZ" loop
-		--	wait for 3 * clk_period;
-		--end loop;
-
-		control_bus <= "00000001";
-		data_bus <= id;
-		wait for clk_period;
-		data_bus <= "00000000";
-		wait for clk_period;
-		data_bus <= data;
-		wait for clk_period;
-		data_bus <= "00000001";
-		wait for clk_period;
-		control_bus <= "ZZZZZZZZ";
-		data_bus <= "ZZZZZZZZ";
-		
-end send;
-
+constant clk_period : time := 10 ns;
 
 begin
 	
 	stim_proc: process
 	begin
-		wait for 5 * clk_period;
-
-		-- send to slaveB
-		send(control_in => control_in, control_bus => control_bus, data_bus => data_bus, id => "00000010", data => "10101010");
-
 		wait for clk_period;
 
-		send(control_in => control_in, control_bus => control_bus, data_bus => data_bus, id => "00000001", data => "11111111");
-	
+		-- slaveA => slaveB
+		sender <= "00000001";
+		id_tosend <= "00000010";
+		data_tosend <= "10101010";
+
+		-- slaveB => slaveC
+		--sender <= "00000010";
+		--id_tosend <= "00000011";
+		--data_tosend <= "10101010";
+
+		wait for 100 * clk_period;
+
+	end process;
+
+	sending_process: process(data_tosend, control_in)
+	begin
+		if control_in /= "00000001" and data_tosend /= "00000000" then
+			control_bus <= "00000001";
+			sending <= '1';
+		end if;
 	end process;
 
 	stateadvance: process(clk)
@@ -88,49 +86,69 @@ begin
 		if rising_edge(clk) then
 			data_in <= data_bus;
 			current_s <= next_s;
+		else
+			data_bus <= tmp_data_bus;
 		end if;
 	end process;
 
-	nextstate: process(current_s, data_in, control_in)
+	nextstate: process(current_s, data_in)
 		variable received : std_logic_vector (7 downto 0) := (others => '0');
 	begin
 
 	case current_s is
 		when IDLE =>
 			vstate <= "000001";
-			
-			if data_in = identifier then
-				next_s <= START;
-			elsif sending = '1' then
+
+			if sender = identifier and sending = '1' then
 				next_s <= WRITE;
+			elsif data_in = identifier and sending /= '0' then
+				next_s <= RECEIVE_START;
 			else
 				next_s <= IDLE;
 			end if;
 
 		when WRITE =>
-			vstate <= "100000";
-			if sending /= '1' then
-				next_s <= IDLE;
-			end if;
+			vstate <= "000010";
+			result_reg <= id_tosend;
+			next_s <= SEND_START;
 
-		when START =>
-			vstate <= "000000";
+		when SEND_START =>
+			vstate <= "000011";
+			result_reg <= "00000000";
+			next_s <= SEND_DATA;
 
-			-- bit 0, 8 data bits, bit 1
+		when SEND_DATA =>
+			vstate <= "000100";
+			result_reg <= data_tosend;
+			next_s <= SEND_STOP;
+
+		when SEND_STOP =>
+			vstate <= "000101";
+			result_reg <= "00000001";
+			next_s <= IDLE;
+
+		when SEND_END =>
+			vstate <= "000110";
+			--sending <= '0';
+			next_s <= IDLE;
+
+		when RECEIVE_START =>
+		-- bit 0, 8 data bits, bit 1
+			vstate <= "001010";
 			case data_in is
 				when "00000000" => 
-					next_s <= DATA;
+					next_s <= RECEIVE_DATA;
 				when others => 
 					next_s <= IDLE;
 			end case;
 
-		when DATA =>
-			vstate <= "000010";
+		when RECEIVE_DATA =>
+			vstate <= "001011";
 			received := data_in;
-			next_s <= STOP;
+			next_s <= RECEIVE_STOP;
 
-		when STOP =>
-			vstate <= "000100";
+		when RECEIVE_STOP =>
+			vstate <= "001100";
 			case data_in is
 				when "00000001" => 
 					next_s <= IDLE;
@@ -148,5 +166,7 @@ begin
 	end process;
 
 	state <= vstate;
+
+	tmp_data_bus <= result_reg when sending = '1' and sender = identifier else "ZZZZZZZZ";
 
 end Behavioral;
